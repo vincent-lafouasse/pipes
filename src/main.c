7443eb52a09826a336b376c4e4e6d5347662b5d0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-13 00:14:47 +0200

    dummy error handling

--- src/main.c
+++ src/main.c
@@ -1,12 +1,24 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "error/t_error.h"
+
+static void cleanup_exit(t_error error);
 
 int main(void)
 {
 	int process_id = fork();
 
+	if (process_id < 0)
+		cleanup_exit(FORK_ERROR);
 	if (process_id == 0)
 		printf("hello from main process\n");
 	else
 		printf("hello from child process\n");
+}
+
+static void cleanup_exit(t_error error)
+{
+	(void)error;
+	exit(EXIT_FAILURE);
 }
