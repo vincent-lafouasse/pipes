6da52313f79e164a71bbd99f6557be0fed7b5889
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-17 16:08:57 +0200

    duplicate args

--- src/command/load_command.c
+++ src/command/load_command.c
@@ -3,18 +3,59 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "libft/ft_string.h"
+#include "libft/string.h"
 
 char* full_path(const char* dir, const char* file);
 t_error locate_command(const char* name, const t_env* env, char** cmd_out);
 
-t_error load_command(char* name, char** args, const t_env* env,t_command* cmd_out)
+void free_str_array(char** arr, size_t n)
+{
+	size_t i;
+
+	if (!arr)
+		return ;
+	i = 0;
+	while (i < n)
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
+char** duplicate_args(char** args, size_t n_args)
+{
+	char** out;
+	size_t i;
+
+	out = malloc((1 + n_args) * sizeof(char*));
+	if (!out)
+		return NULL;
+	i = 0;
+	while (i < n_args)
+	{
+		out[i] = ft_strdup(args[i]);
+		if (!out[i])
+		{
+			free_str_array(out, i);
+			return NULL;
+		}
+		i++;
+	}
+	out[i] = NULL;
+	return out;
+}
+
+t_error load_command(char* name, char** args, size_t n_args, const t_env* env,t_command* cmd_out)
 {
 	t_error err;
 
 	err = locate_command(name, env, &cmd_out->location);
 	if (err != NO_ERROR)
 		return err;
-	cmd_out->args = args;
+	cmd_out->args = duplicate_args(args, n_args);
+	if (cmd_out->args == NULL)
+		return OOM_ERROR;
 	return NO_ERROR;
 }
 
