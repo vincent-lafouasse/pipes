0bfc917d14c818c7c22f014d19d7e1b381a8ed10
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-17 23:20:23 +0200

    implement convenience closing functions for important fds

--- src/files/close_files.c
+++ src/files/close_files.c
@@ -9,3 +9,33 @@ void close_files(t_files* files)
 	close_fd(&files->pipe[0]);
 	close_fd(&files->pipe[1]);
 }
+
+
+void close_pipe_input(t_files* files)
+{
+	if (!files)
+		return;
+	close_fd(&files->pipe[READ]);
+}
+
+void close_pipe_output(t_files* files)
+{
+	if (!files)
+		return;
+	close_fd(&files->pipe[WRITE]);
+}
+
+void close_infile(t_files* files)
+{
+	if (!files)
+		return;
+	close_fd(&files->in_fd);
+}
+
+void close_outfile(t_files* files)
+{
+	if (!files)
+		return;
+	close_fd(&files->out_fd);
+}
+
--- src/files/t_files.h
+++ src/files/t_files.h
@@ -2,6 +2,9 @@
 
 #include "error/t_error.h"
 
+#define READ 0
+#define WRITE 1
+
 typedef struct s_files
 {
 	int in_fd;
@@ -12,3 +15,8 @@ typedef struct s_files
 t_error open_files(const char* infile, const char* outfile, t_files* files);
 void close_files(t_files* files);
 void close_fd(int* fd);
+
+void close_pipe_input(t_files* files);
+void close_pipe_output(t_files* files);
+void close_infile(t_files* files);
+void close_outfile(t_files* files);
