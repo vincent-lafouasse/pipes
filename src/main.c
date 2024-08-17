d1bc0e1520b8bb82f09c39aced92a04cbfd8da05
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-17 22:38:45 +0200

    implement a basic pipe

--- src/main.c
+++ src/main.c
@@ -96,7 +96,7 @@ int main(int ac, char** av, char** sys_env)
 {
 	t_pipex pipex;
 	t_io_files files;
-	pid_t pid;
+	pid_t pid[2];
 	t_error err;
 
 	err = load_pipex_input(ac, av, sys_env, &pipex);
@@ -107,19 +107,26 @@ int main(int ac, char** av, char** sys_env)
 	if (err != NO_ERROR)
 		cleanup_exit(&pipex, NULL, err);
 
-	pid = fork();
-	if (pid == 0)
+	pid[0] = fork();
+	if (pid[0] == 0)
 	{
+		// child process 1
 		close(files.pipe[READ]);
 		dup2(files.in_fd, STDIN_FILENO);
+		close(files.in_fd);
+		dup2(files.pipe[WRITE], STDOUT_FILENO);
+		close(files.pipe[WRITE]);
 		execve(pipex.cmd1->location, pipex.cmd1->args, sys_env);
 	}
 	else
 	{
 		close(files.pipe[WRITE]);
+		dup2(files.out_fd, STDOUT_FILENO);
+		close(files.out_fd);
+		dup2(files.pipe[READ], STDIN_FILENO);
+		close(files.pipe[READ]);
 		wait(NULL);
 		printf("\n");
-		dup2(files.out_fd, STDOUT_FILENO);
 		execve(pipex.cmd2->location, pipex.cmd2->args, sys_env);
 	}
 	cleanup(&pipex, &files);
