7be51645b5b555834ca8e69f5428f70e50dd9ad6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-17 19:14:21 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -16,28 +16,44 @@ typedef struct s_pipex {
 	t_env env;
 } t_pipex;
 
+t_pipex t_pipex_init(void)
+{
+	return (
+		(t_pipex){
+		.cmd1 = NULL,
+		.cmd2 = NULL,
+		.infile = NULL,
+		.outfile = NULL,
+		.env = (t_env){.sys_env = NULL, .path = NULL}
+	}
+	);
+}
+
 t_error load_pipex_input(int ac, char** av, char** sys_env, t_pipex* out)
 {
 	t_error err;
 
 	if (ac != 5)
 		return  BAD_N_ARGS_ERROR;
-	out->infile = av[1];
-	out->outfile = av[4];
-	out->cmd1 = NULL;
-	out->cmd2 = NULL;
-	err = load_env((const char**)sys_env, &out->env);
+
+	*out = t_pipex_init();
+
+	err = load_env(sys_env, &out->env);
 	if (err != NO_ERROR)
 		return err;
+
 	out->cmd1 = malloc(sizeof(*out->cmd1));
 	if (!out->cmd1)
 		return OOM_ERROR;
+
 	err = load_command(av[2], &out->env, out->cmd1);
 	if (err != NO_ERROR)
 		return err;
+
 	out->cmd2 = malloc(sizeof(*out->cmd2));
 	if (!out->cmd2)
 		return OOM_ERROR;
+
 	return load_command(av[3], &out->env, out->cmd2);
 }
 
