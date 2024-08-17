9ec3fc658b2ab456d97ac627ffbdc69e9faa07b2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-17 15:26:53 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -13,6 +13,39 @@ typedef struct s_command {
 	int out_fd;
 } t_command;
 
+char* full_path(const char* dir, const char* file);
+t_error locate_command(const char* name, const t_env* env, char** cmd_out);
+t_error load_command(t_command* cmd_out);
+
+static void cleanup_exit(t_error error);
+
+int main(int ac, char** av, char** sys_env)
+{
+	(void)ac;
+	t_env env;
+	t_error err;
+
+	err = load_env((const char**)sys_env, &env);
+	if (err != NO_ERROR)
+		cleanup_exit(err);
+
+	char* command_name = av[1];
+	char** args = av + 2;
+	char* command_location;
+	err = locate_command(command_name, &env, &command_location);
+	if (err != NO_ERROR)
+		cleanup_exit(err);
+
+	if (execve(command_location, args, sys_env) == -1)
+		cleanup_exit(DUMMY_ERROR);
+}
+
+static void cleanup_exit(t_error error)
+{
+	(void)error;
+	exit(EXIT_FAILURE);
+}
+
 char* full_path(const char* dir, const char* file)
 {
 	char* dir_with_slash;
@@ -37,46 +70,18 @@ t_error locate_command(const char* name, const t_env* env, char** cmd_out)
 	int i = 0;
 
 	dir = env->path[0];
-	printf("hhhhhhhhhhhhhhhhhhhhhhhhhhh");
 	while (dir)
 	{
 		candidate = full_path(dir, name);
 		if (!candidate)
 			return OOM_ERROR;
-		printf("%s\n", candidate);
+		if (access(candidate, X_OK) == 0)
+		{
+			*cmd_out = candidate;
+			return NO_ERROR;
+		}
 		free(candidate);
 		dir = env->path[++i];
 	}
-	printf("bbbbbbbbbbbbbbbbbbbbbbbbbhh");
 	return FILE_NOT_FOUND_ERROR;
-}
-
-static void cleanup_exit(t_error error);
-
-int main(int ac, char** av, char** sys_env)
-{
-	(void)ac;
-	(void)av;
-	t_env env;
-	t_error err;
-
-	err = load_env((const char**)sys_env, &env);
-	if (err != NO_ERROR)
-		cleanup_exit(err);
-
-	char* command_name = "ls";
-	char* args[] = {"-l", NULL};
-	char* command_location;
-	err = locate_command(command_name, &env, &command_location);
-	if (err != NO_ERROR)
-		cleanup_exit(err);
-
-	if (execve("/bin/ls", args, sys_env) == -1)
-		cleanup_exit(DUMMY_ERROR);
-}
-
-static void cleanup_exit(t_error error)
-{
-	(void)error;
-	exit(EXIT_FAILURE);
 }
