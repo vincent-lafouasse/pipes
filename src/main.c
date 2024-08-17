a11b544a75b5cd870b5ce0e3a345ecdcdca778e1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-17 23:28:27 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -9,9 +9,6 @@
 
 #include "libft/ft_io.h"
 
-#define READ 0
-#define WRITE 1
-
 static void cleanup(t_pipex* pipex, t_files* files);
 static void cleanup_exit(t_pipex* pipex, t_files* files, t_error error);
 
@@ -33,27 +30,23 @@ int main(int ac, char** av, char** sys_env)
 	pid[0] = fork();
 	if (pid[0] == 0)
 	{
-		close(files.pipe[READ]);
-		dup2(files.in_fd, STDIN_FILENO);
-		close(files.in_fd);
-		dup2(files.pipe[WRITE], STDOUT_FILENO);
-		close(files.pipe[WRITE]);
+		close_pipe_input(&files);
+		redirect((t_redirect){.from = STDIN_FILENO, .to = files.in_fd});
+		redirect((t_redirect){.from = STDOUT_FILENO, .to = files.pipe[WRITE]});
 		execve(pipex.cmd1->location, pipex.cmd1->args, sys_env);
 	}
 	
 	pid[1] = fork();
 	if (pid[1] == 0)
 	{
-		close(files.pipe[WRITE]);
-		dup2(files.out_fd, STDOUT_FILENO);
-		close(files.out_fd);
-		dup2(files.pipe[READ], STDIN_FILENO);
-		close(files.pipe[READ]);
+		close_pipe_output(&files);
+		redirect((t_redirect){.from = STDIN_FILENO, .to = files.pipe[READ]});
+		redirect((t_redirect){.from = STDOUT_FILENO, .to = files.out_fd});
 		execve(pipex.cmd2->location, pipex.cmd2->args, sys_env);
 	}
 
-	close(files.pipe[0]);
-	close(files.pipe[1]);
+	close_fd(&files.pipe[0]);
+	close_fd(&files.pipe[1]);
 	waitpid(pid[0], NULL, 0);
 	waitpid(pid[1], NULL, 0);
 	cleanup(&pipex, &files);
