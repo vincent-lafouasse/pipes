8b7e4253185127ae806c14313f0b0ceeff015f21
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-17 14:16:06 +0200

    implement ft_str_starts_with

--- lib/libft/include/libft/ft_string.h
+++ lib/libft/include/libft/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 10:14:24 by poss              #+#    #+#             */
-/*   Updated: 2024/03/30 10:16:20 by poss             ###   ########.fr       */
+/*   Updated: 2024/08/17 14:13:59 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FT_STRING_H
 
 # include "types.h"
+# include <stdbool.h>
 
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
@@ -22,5 +23,6 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+bool	ft_str_starts_with(const char* s, const char* pattern);
 
 #endif
---
+++ lib/libft/src/extension/string/ft_str_starts_with.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_starts_with.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/17 14:14:07 by poss              #+#    #+#             */
+/*   Updated: 2024/08/17 14:15:36 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/ft_string.h"
+#include "libft/string.h"
+
+bool	ft_str_starts_with(const char* s, const char* pattern)
+{
+	if (!s || !pattern)
+		return false;
+	if (ft_strlen(pattern) > ft_strlen(s))
+		return false;
+	while (*pattern)
+	{
+		if (*pattern != *s)
+			return false;
+		s++;
+		pattern++;
+	}
+	return true;
+}
