02a09870e3f65154e16618df63bdff0c9cd801f0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-17 23:34:49 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -11,11 +11,39 @@
 static void	cleanup(t_pipex *pipex, t_files *files);
 static void	cleanup_exit(t_pipex *pipex, t_files *files, t_error error);
 
+t_error execute(t_pipex* pipex, t_files* files)
+{
+	pid_t pid[2];
+
+	pid[0] = fork();
+	if (pid[0] == 0)
+	{
+		close_pipe_input(files);
+		redirect((t_redirect){.from = STDIN_FILENO, .to = files->in_fd});
+		redirect((t_redirect){.from = STDOUT_FILENO, .to = files->pipe[WRITE]});
+		execve(pipex->cmd1->location, pipex->cmd1->args, pipex->env.sys_env);
+	}
+
+	pid[1] = fork();
+	if (pid[1] == 0)
+	{
+		close_pipe_output(files);
+		redirect((t_redirect){.from = STDIN_FILENO, .to = files->pipe[READ]});
+		redirect((t_redirect){.from = STDOUT_FILENO, .to = files->out_fd});
+		execve(pipex->cmd2->location, pipex->cmd2->args, pipex->env.sys_env);
+	}
+
+	close_fd(&files->pipe[0]);
+	close_fd(&files->pipe[1]);
+	waitpid(pid[0], NULL, 0);
+	waitpid(pid[1], NULL, 0);
+	return NO_ERROR;
+}
+
 int	main(int ac, char **av, char **sys_env)
 {
 	t_pipex	pipex;
 	t_files	files;
-	pid_t	pid[2];
 	t_error	err;
 
 	err = load_pipex_input(ac, av, sys_env, &pipex);
@@ -26,28 +54,9 @@ int	main(int ac, char **av, char **sys_env)
 	if (err != NO_ERROR)
 		cleanup_exit(&pipex, NULL, err);
 
-	pid[0] = fork();
-	if (pid[0] == 0)
-	{
-		close_pipe_input(&files);
-		redirect((t_redirect){.from = STDIN_FILENO, .to = files.in_fd});
-		redirect((t_redirect){.from = STDOUT_FILENO, .to = files.pipe[WRITE]});
-		execve(pipex.cmd1->location, pipex.cmd1->args, sys_env);
-	}
-
-	pid[1] = fork();
-	if (pid[1] == 0)
-	{
-		close_pipe_output(&files);
-		redirect((t_redirect){.from = STDIN_FILENO, .to = files.pipe[READ]});
-		redirect((t_redirect){.from = STDOUT_FILENO, .to = files.out_fd});
-		execve(pipex.cmd2->location, pipex.cmd2->args, sys_env);
-	}
-
-	close_fd(&files.pipe[0]);
-	close_fd(&files.pipe[1]);
-	waitpid(pid[0], NULL, 0);
-	waitpid(pid[1], NULL, 0);
+	err = execute(&pipex, &files);
+	if (err != NO_ERROR)
+		cleanup_exit(&pipex, &files, err);
 	cleanup(&pipex, &files);
 }
 
