96e4854f284b64aa8a1a30c570d7ea590bf3d004
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-17 22:47:26 +0200

    restructure

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "env/t_env.h"
 #include "log/log.h"
 #include "command/t_command.h"
+#include "t_pipex/t_pipex.h"
 
 #include "libft/ft_io.h"
 
@@ -39,54 +40,6 @@ t_error open_files(const char* infile, const char* outfile, t_io_files* files)
 		return  PIPE_ERROR;
 	}
 	return NO_ERROR;
-}
-
-typedef struct s_pipex {
-	t_command* cmd1;
-	t_command* cmd2;
-	char* infile;
-	char* outfile;
-	t_env env;
-} t_pipex;
-
-t_pipex t_pipex_init(void)
-{
-	return (
-		(t_pipex){
-		.cmd1 = NULL,
-		.cmd2 = NULL,
-		.infile = NULL,
-		.outfile = NULL,
-		.env = (t_env){.sys_env = NULL, .path = NULL}
-	}
-	);
-}
-
-t_error load_pipex_input(int ac, char** av, char** sys_env, t_pipex* out)
-{
-	t_error err;
-
-	if (ac != 5)
-		return  BAD_N_ARGS_ERROR;
-
-	*out = t_pipex_init();
-
-	err = load_env(sys_env, &out->env);
-	if (err != NO_ERROR)
-		return err;
-
-	out->cmd1 = malloc(sizeof(*out->cmd1));
-	out->cmd2 = malloc(sizeof(*out->cmd2));
-	if (!out->cmd1 || !out->cmd2)
-		return OOM_ERROR;
-
-	err = load_command(av[2], &out->env, out->cmd1);
-	if (err != NO_ERROR)
-		return err;
-
-	out->infile = av[1];
-	out->outfile = av[4];
-	return load_command(av[3], &out->env, out->cmd2);
 }
 
 static void cleanup(t_pipex* pipex, t_io_files* files);
---
+++ src/t_pipex/t_pipex.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "command/t_command.h"
+
+typedef struct s_pipex {
+	t_command* cmd1;
+	t_command* cmd2;
+	char* infile;
+	char* outfile;
+	t_env env;
+} t_pipex;
+
+t_error load_pipex_input(int ac, char** av, char** sys_env, t_pipex* out);
