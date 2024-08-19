472d18ddc8f9efc1218e17b7c335bfc4fdf2e868
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-19 18:57:25 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -13,28 +13,37 @@
 static void	cleanup(t_pipex *pipex, t_files *files);
 static void	cleanup_exit(t_pipex *pipex, t_files *files, t_error error);
 
+static void launch_process1(t_pipex* pipex, t_files* files)
+{
+	close_pipe_input(files);
+	redirect((t_redirect){.from = STDIN_FILENO, .to = files->in_fd});
+	redirect((t_redirect){.from = STDOUT_FILENO, .to = files->pipe[WRITE]});
+	execve(pipex->cmd1->location, pipex->cmd1->args, pipex->env.sys_env);
+}
+
+static void launch_process2(t_pipex* pipex, t_files* files)
+{
+	close_pipe_output(files);
+	redirect((t_redirect){.from = STDIN_FILENO, .to = files->pipe[READ]});
+	redirect((t_redirect){.from = STDOUT_FILENO, .to = files->out_fd});
+	execve(pipex->cmd2->location, pipex->cmd2->args, pipex->env.sys_env);
+}
+
+
 t_error execute(t_pipex* pipex, t_files* files)
 {
 	pid_t pid[2];
 
 	pid[0] = fork();
+	if (pid[0] < 0)
+		return FORK_ERROR;
 	if (pid[0] == 0)
-	{
-		close_pipe_input(files);
-		redirect((t_redirect){.from = STDIN_FILENO, .to = files->in_fd});
-		redirect((t_redirect){.from = STDOUT_FILENO, .to = files->pipe[WRITE]});
-		execve(pipex->cmd1->location, pipex->cmd1->args, pipex->env.sys_env);
-	}
-
+		launch_process1(pipex, files);
 	pid[1] = fork();
+	if (pid[1] < 0)
+		return FORK_ERROR;
 	if (pid[1] == 0)
-	{
-		close_pipe_output(files);
-		redirect((t_redirect){.from = STDIN_FILENO, .to = files->pipe[READ]});
-		redirect((t_redirect){.from = STDOUT_FILENO, .to = files->out_fd});
-		execve(pipex->cmd2->location, pipex->cmd2->args, pipex->env.sys_env);
-	}
-
+		launch_process2(pipex, files);
 	close_fd(&files->pipe[0]);
 	close_fd(&files->pipe[1]);
 	waitpid(pid[0], NULL, 0);
