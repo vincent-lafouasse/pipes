03fb1cdf404ca8adcb65cf4ec1f4579ee950c839
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-19 14:45:43 +0200

    update error variant reprs

--- src/error/error_repr.c
+++ src/error/error_repr.c
@@ -1,16 +1,17 @@
 #include "t_error.h"
 
 const char* reprs[] = {
-	"NO ERROR",
-	"FORK",
-	"PIPE",
-	"NULL PARAM",
-	"DUMMY",
-	"NO PATH FOUND",
-	"FILE NOT FOUND",
-	"FILE NOT EXEC",
-	"OOM",
+	"NO_ERROR",
+	"FORK_ERROR",
+	"PIPE_ERROR",
+	"NULL_PARAM_ERROR",
+	"DUMMY_ERROR",
+	"NO_PATH_FOUND_ERROR",
+	"FILE_NOT_FOUND_ERROR",
+	"FILE_NOT_EXEC_ERROR",
+	"OOM_ERROR",
 	"BAD_N_ARGS_ERROR",
+	"CANT_OPEN_FILE_ERROR",
 };
 
 const char* error_repr(t_error err)
