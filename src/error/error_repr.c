4333da18d5899cb773b5ef4031698569f7f2c0ff
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-20 20:46:06 +0200

    refactor

--- src/error/error_repr.c
+++ src/error/error_repr.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 12:32:37 by poss              #+#    #+#             */
-/*   Updated: 2024/08/20 20:40:19 by poss             ###   ########.fr       */
+/*   Updated: 2024/08/20 20:45:54 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,23 @@ const char	*reprs[] = {
 
 const char	*error_repr(t_error err)
 {
-	return (reprs[err]);
+	if (err == FORK_ERROR)
+		return "Error spawning new process";
+	if (err == PIPE_ERROR)
+		return "Error opening UNIX pipe";
+	if (err == NULL_PARAM_ERROR)
+		return "Parameter was NULL";
+	if (err == NO_PATH_FOUND_ERROR)
+		return "PATH not found in system env";
+	if (err == COMMAND_NOT_AVAILABLE_ERROR)
+		return "Couldn't access command";
+	if (err == FILE_NOT_EXEC_ERROR)
+		return "Command is not executable";
+	if (err == OOM_ERROR)
+		return "Out of memory";
+	if (err == BAD_N_ARGS_ERROR)
+		return "Usage ./pipex infile cmd1 cmd2 outfile";
+	if (err == CANT_OPEN_FILE_ERROR)
+		return "Error opening file";
+	return "Unrecognised error";
 }
